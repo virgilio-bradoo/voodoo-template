22a97bcc2a0ea546be354f0fb6f0fb0b247cbe76
Arthur Vuillard <arthur.vuillard@gmail.com>
2014-04-17 10:02:04 +0200

    Initial commit of the module

---
+++ logistiflex_customs/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Logistfilex customs',
+    'version': '1',
+    'category': 'Client',
+    'description': """
+Add customizations for Logistiflex
+""" ,
+    'author': 'Arthur Vuillard',
+    'website': 'http://www.akretion.com',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo_xml': [],
+    'test': [],
+    'installable': True,
+    'active': False,
+}
